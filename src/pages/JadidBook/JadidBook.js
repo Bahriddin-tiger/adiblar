2323d171659a3f16cf48ac54ade912434833d178
Bahriddin-tiger <934045436b@gmail.com>
2022-08-27 11:50:35 +0500

    Initiall commit

--- src/pages/JadidBook/JadidBook.js
+++ src/pages/JadidBook/JadidBook.js
@@ -1,13 +1,11 @@
 
 import "./JadidBook.scss"
-import { useContext, useEffect } from "react"
+import {  useEffect } from "react"
 import { useState } from "react"
 import axios from "axios"
 import { BookItem} from "../../components"
-import { AuthContext } from "../../context/AuthContext"
 export const JadidBook = () =>{
     const [jadidBooklar, setJadidBooklar] = useState("")
-    const {token}=useContext(AuthContext)
     useEffect(()=>{
         axios.get('https://book-service-layer.herokuapp.com/book/genreId/2')
         .then((res)=> setJadidBooklar(res.data))
